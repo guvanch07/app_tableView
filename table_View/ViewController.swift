84bb1c04b8bd149eec147cdc5f19896e0a7dff94
guvanch07 <guvanchamanov777@gmail.com>
2022-06-07 23:12:18 +0300

    first

---
+++ table_View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  table_View
+//
+//  Created by Macbook on 7.06.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
